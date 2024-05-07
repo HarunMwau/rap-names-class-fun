d808648b869c1deef20b1ee296bd95632b31acd5
HarunMwau <dittofarm7@gmail.com>
2024-05-07 23:02:13 +0300

    updated client-side code to display name

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-wytp.onrender.com/api/${rapperName}`) 
+        const response = await fetch(`https://rap-names-class-fun-tpm8.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('h2').innerText = data.birthname
     }catch(error){
         console.log(error)
     }
